3550c78e92d343f947e748820085fe470d25e35e
Vaibhavraj Nath Chauhan <19etccs078@technonjr.org>
2021-06-13 17:29:22 +0530

    Brain of project

---
+++ HandTrackingModule.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import cv2
+import mediapipe as mp
+import time
+class handDetector():
+    def __init__(self,mode=False, maxHands=2,detectionCon=.5,trackCon=.5):
+        #These are the papamter which we will pass to Hands() function 
+        self.mode = mode
+        self.maxHands = maxHands
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+        
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(self.mode,self.maxHands,
+                                        self.detectionCon,self.trackCon)
+        self.mpDraw = mp.solutions.drawing_utils
+        
+    def findHands(self,img,draw=True):
+        imgBGR = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(img)
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img,handLms,
+                                               self.mpHands.HAND_CONNECTIONS)
+        return img
+    
+    def findPosition(self,img,handNo=0,draw=True):
+        lmList = []
+        if self.results.multi_hand_landmarks:
+            myHand = self.results.multi_hand_landmarks[handNo]
+            for id, lm in enumerate(myHand.landmark):
+                h,w,c = img.shape
+                cx,cy = int(w*lm.x), int(h*lm.y)
+                lmList.append([id,cx,cy])
+                if draw:
+                    cv2.circle(img,(cx,cy),15,(255,0,255),cv2.FILLED)
+        return lmList
+
